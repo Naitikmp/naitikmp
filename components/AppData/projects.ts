f3e96f744f99f94bd4b658a8bb00f7fd4fa7187d
Naitikmp <90212814+Naitikmp@users.noreply.github.com>
2025-01-02 02:41:45 +0530

    updations to projects

--- components/AppData/projects.ts
+++ components/AppData/projects.ts
@@ -3,13 +3,36 @@ const PROJECTS_DATA = [
     title: 'Wind turbine Defects detection',
     description: 'Your next Gen-AI based end-to-end solution for Wind Turbine Defects Detection.',
     about:
-      'This is the Web app based dashboard made to identify the defects from the data collected using the UAV(drones) and it inferes that data and provides with the defects detection and also editing and correcting them with the state of the art canvas built in and can finaly make a report out of your detections.',
+    'This is the Web app based dashboard made to identify the defects from the data collected using the UAV(drones) and it inferes that data and provides with the defects detection and also editing and correcting them with the state of the art canvas built in and can finaly make a report out of your detections.',
     link: 'https://turbine-defect-dashboard.vercel.app/',
     imageUrl: 'https://i.ibb.co/LRgf1R9/Untitled-video-Made-with-Clipchamp.gif',
     github: 'https://github.com/Naitikmp/Turbine-Defect-Dashboard',
     type: ['project', 'Corporate', 'tools'],
-    technologies: ['React', 'CSS', 'Javascript', 'Python', 'Flask', 'YOLO' ,'Pytorch' , 'AWS'],
+    technologies: ['React', 'CSS', 'Javascript', 'Python', 'Flask', 'YOLO', 'Pytorch', 'AWS'],
   },
+  {
+    title: 'LogiChat',
+    description: 'LogiChat: RAG-Based Chatbot with Document Uploads',
+    about:
+      'LogiChat is a Retrieval-Augmented Generation (RAG) based chatbot application powered by Large Language Models (LLMs). It enables users to upload their documents and have intelligent, context-aware conversations derived exclusively from their provided data.',
+    link: 'https://github.com/Naitikmp/LogiChat',
+    imageUrl: 'https://i.ibb.co/wwKYQCv/Untitled-video-Made-with-Clipchamp-1.gif',
+    github: 'https://github.com/Naitikmp/LogiChat',
+    type: ['project', 'Corporate', 'tools'],
+    technologies: ['Langchain','OpenAI','RAG', 'LLMs','React', 'CSS', 'Javascript', 'Python', 'Flask', 'Huggingface',  'AWS'],
+  },
+  {
+    title: 'Stock-analysis-app-using-LLMs',
+    description: 'provides intelligent stock analysis and recommendations using LangChain and OpenAI GPT models.',
+    about:
+      'A Demo simple web application that provides intelligent stock analysis and recommendations using LangChain and OpenAI GPT models. The application features a Flask backend for data processing and analysis, coupled with a sleek and simple Next.js frontend for an optimal user experience.',
+    link: 'https://github.com/Naitikmp/Stock-analysis-app-using-LLMs',
+    imageUrl: 'https://i.ibb.co/744ZzZC/Untitled-video-Made-with-Clipchamp-2.gif',
+    github: 'https://github.com/Naitikmp/Stock-analysis-app-using-LLMs',
+    type: ['project', 'Corporate', 'tools'],
+    technologies: ['Langchain','OpenAI','RAG', 'LLMs','React', 'CSS', 'Javascript', 'Python', 'Flask', 'Huggingface',  'AWS'],
+  },
+
   {
     title: 'Aniframes',
     about: 'this is a marketplace that allows you to experience stunning anime frames and buy them online',
@@ -19,7 +42,7 @@ const PROJECTS_DATA = [
     github: 'https://github.com/Naitikmp/Aniframes-Client',
     imageUrl: 'https://i.ibb.co/vBTpzBN/Screen-Recording2024-10-31173036-ezgif-com-video-to-gif-converter.gif',
     type: ['project', 'open-source', 'tools'],
-    technologies: ['Typescript', 'Rollup', 'React', 'Nextjs' , 'AWS'],
+    technologies: ['Typescript', 'Rollup', 'React', 'Nextjs', 'AWS'],
   },
   {
     title: 'Cold Mailing using Llama 3.1',
@@ -32,7 +55,7 @@ const PROJECTS_DATA = [
     type: ['project'],
     technologies: ['Streamlit', 'Langchain', 'Llama 3.1', 'llms', 'Cloud  rendering'],
   },
-    {
+  {
     title: 'Movie Caatalogue web app',
     description:
       'this is a simple movie catalogue app',
@@ -42,7 +65,7 @@ const PROJECTS_DATA = [
     imageUrl:
       'https://i.ibb.co/j5F1nZ8/Screenshot-2024-10-31-182607.png',
     type: ['project'],
-    technologies: ['React', 'html','css', 'Javascript'],
+    technologies: ['React', 'html', 'css', 'Javascript'],
   },
 
 ];
